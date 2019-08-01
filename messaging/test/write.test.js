f175de50b14d6fb197a6a00d7840400a18076b4c
Mario Pareja <pareja.mario@gmail.com>
2019-08-01 18:35:49 -0400

    messaging: implement write

---
+++ messaging/test/write.test.js
@@ -0,0 +1,91 @@
+const createLog = require('../../test/test-log')
+const createWrite = require('../write')
+const { toWriteMessageData } = require('../message-transforms')
+const {
+  exampleMessage,
+  exampleStreamName
+} = require('../../message-store/examples')
+
+const A_POSITION = 666
+
+describe('write', () => {
+  let log, store, write
+  beforeEach(() => {
+    log = createLog()
+    store = { write: jest.fn().mockResolvedValue(A_POSITION) }
+    write = createWrite({ log, store })
+  })
+
+  describe('single message', () => {
+    let message, streamName
+    beforeEach(async () => {
+      message = exampleMessage()
+      streamName = exampleStreamName()
+    })
+
+    it('writes message to store', async () => {
+      await write(message, streamName)
+
+      const messageData = toWriteMessageData(message)
+      expect(store.write).toHaveBeenCalledWith([messageData], streamName, undefined)
+    })
+
+    it('returns position', async () => {
+      const result = await write(message, streamName)
+
+      expect(result).toEqual(A_POSITION)
+    })
+
+    it('success is logged', async () => {
+      await write(message, streamName)
+
+      expect(log.info).toHaveBeenCalled()
+    })
+
+    describe('expectedVersion provided', () => {
+      it('writes message to store with expected version', async () => {
+        const expectedVersion = 123
+
+        await write(message, streamName, { expectedVersion })
+
+        const messageData = toWriteMessageData(message)
+        expect(store.write).toHaveBeenCalledWith(
+          [messageData], streamName, expectedVersion)
+      })
+    })
+  })
+
+  describe('batch of messages', () => {
+    let messages
+    beforeEach(async () => {
+      messages = [exampleMessage(), exampleMessage()]
+    })
+
+    it('writes multiple messages to store', async () => {
+      const streamName = exampleStreamName()
+
+      await write(messages, streamName)
+
+      const messageDataBatch = [
+        toWriteMessageData(messages[0]),
+        toWriteMessageData(messages[1])
+      ]
+
+      expect(store.write).toHaveBeenCalledWith(messageDataBatch, streamName, undefined)
+    })
+  })
+
+  describe('invalid message', () => {
+    it('is an error', async () => {
+      const streamName = exampleStreamName()
+
+      const promise = write(null, streamName)
+
+      await expect(promise).rejects.toEqual(new Error(
+        'messaging write: one or more messages were invalid'))
+      await expect(promise).rejects.toMatchObject({
+        inner: expect.anything()
+      })
+    })
+  })
+})
---
+++ messaging/write.js
@@ -0,0 +1,33 @@
+const operationError = require('../errors/operation-error')
+const { toWriteMessageData } = require('./message-transforms')
+
+const writeError = operationError('messaging write')
+
+module.exports = ({ log, store }) => async (messageOrBatch, streamName, { expectedVersion } = {}) => {
+  const info = {
+    count: Array.isArray(messageOrBatch) ? messageOrBatch.length : 1,
+    expectedVersion,
+    streamName
+  }
+  log.debug(info, 'messaging write: starting')
+
+  const messages = Array.isArray(messageOrBatch) ? messageOrBatch : [messageOrBatch]
+  const data = transformMessages(messages)
+
+  const position = store.write(data, streamName, expectedVersion)
+
+  log.info({
+    ...info,
+    types: data.map(d => d.type)
+  }, 'messaging write: successful')
+
+  return position
+}
+
+const transformMessages = (messages) => {
+  try {
+    return messages.map(toWriteMessageData)
+  } catch (err) {
+    throw writeError('one or more messages were invalid', err)
+  }
+}
