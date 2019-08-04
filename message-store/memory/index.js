a94ca2b7cf6ed3fa57351f54f36b0a38a5898f90
Mario Pareja <pareja.mario@gmail.com>
2019-08-04 05:46:18 -0400

    message-store: memory: further flush out implementation

--- message-store/memory/index.js
+++ message-store/memory/index.js
@@ -1,5 +1,13 @@
+const cloneDeep = require('lodash.clonedeep')
+const operationError = require('../../errors/operation-error')
+const { uuid } = require('../../identifier')
+
+const writeError = operationError('message-store write')
+
 module.exports = () => {
   const streams = {}
+  const messageIds = {}
+  let globalPosition = 0
 
   const read = async function * (streamName, position) {
     position = position || 0
@@ -11,17 +19,40 @@ module.exports = () => {
     }
   }
 
-  const write = (msgOrBatch, streamName) => {
+  const write = async (msgOrBatch, streamName, expectedVersion) => {
     let stream = streams[streamName]
     if (!stream) {
       stream = []
       streams[streamName] = stream
     }
 
-    const batch = Array.isArray(msgOrBatch) ? msgOrBatch : [msgOrBatch]
-    for (const message of batch) {
-      stream.push(message)
+    const currentVersion = stream.length - 1
+    if (typeof expectedVersion === 'number' && expectedVersion !== currentVersion) {
+      throw writeError(`Wrong expected version: ${expectedVersion} (Stream: ${streamName}, Stream Version: ${currentVersion})`)
     }
+    let lastPosition
+
+    const batch = Array.isArray(msgOrBatch) ? msgOrBatch : [msgOrBatch]
+    for (const inputMessage of batch) {
+      const id = inputMessage.id || uuid()
+      if (messageIds[id]) {
+        throw writeError(`duplicate message id: ${id}`)
+      }
+
+      const message = cloneDeep(inputMessage)
+      message.id = message.id || uuid()
+      message.position = stream.length
+      message.globalPosition = globalPosition++
+      message.time = new Date()
+      message.streamName = streamName
+
+      lastPosition = message.position
+
+      stream.push(message)
+      messageIds[message.id] = true
+    }
+
+    return lastPosition
   }
 
   return { read, write }
--- message-store/memory/test/message-store-memory.test.js
+++ message-store/memory/test/message-store-memory.test.js
@@ -1,79 +1,233 @@
 const createMessageStore = require('../index')
+const uuidValidate = require('uuid-validate')
 const {
-  exampleMessageData
+  exampleMessageData,
+  exampleStreamName
 } = require('../../examples')
 
 describe('message-store-memory', () => {
+  let store
+  beforeEach(() => {
+    store = createMessageStore()
+  })
+
+  const read = (...args) => {
+    return readFrom(store, ...args)
+  }
+
+  const readFrom = async (aStore, streamName, position) => {
+    const found = []
+    for await (const message of aStore.read(streamName, position)) {
+      found.push(message)
+    }
+    return found
+  }
+
   describe('reading stream that does not exist', () => {
     it('returns no messages', async () => {
-      const store = createMessageStore()
-      const found = []
-
-      for await (const message of store.read('stream-123')) {
-        found.push(message)
-      }
+      const found = await read('stream-123')
 
       expect(found).toHaveLength(0)
     })
   })
 
+  describe('write message to stream', () => {
+    let position, streamName, writeMessage
+    beforeEach(async () => {
+      writeMessage = exampleMessageData()
+      streamName = exampleStreamName()
+      position = await store.write(writeMessage, streamName)
+    })
+
+    it('can be retrieved', async () => {
+      const found = await read(streamName)
+      expect(found).toHaveLength(1)
+      expect(found[0]).toEqual({
+        id: writeMessage.id,
+        type: writeMessage.type,
+        data: writeMessage.data,
+        metadata: writeMessage.metadata,
+        streamName,
+        position: 0,
+        globalPosition: expect.any(Number),
+        time: expect.any(Date)
+      })
+    })
+
+    it('returns the position', () => {
+      expect(position).toBe(0)
+    })
+  })
+
+  describe('stream without id', () => {
+    it('writes the category as the stream name', async () => {
+      const streamName = exampleStreamName(null, 'none')
+      await store.write(exampleMessageData(), streamName)
+
+      const found = await read(streamName)
+
+      expect(found[0].streamName).toBe(streamName)
+    })
+  })
+
+  describe('message without id', () => {
+    it('generates id', async () => {
+      const streamName = exampleStreamName()
+      const writeMessage = exampleMessageData()
+      delete writeMessage.id
+      await store.write(writeMessage, streamName)
+
+      const readMessage = (await read(streamName))[0]
+
+      expect(uuidValidate(readMessage.id)).toBe(true)
+    })
+  })
+
   describe('write seperate messages to stream', () => {
-    let store, message1, message2
-    beforeEach(() => {
+    let message1, message2
+    beforeEach(async () => {
       message1 = exampleMessageData()
       message2 = exampleMessageData()
-      store = createMessageStore()
-      store.write(message1, 'stream-123')
-      store.write(message2, 'stream-123')
+      await store.write(message1, 'stream-123')
+      await store.write(message2, 'stream-123')
     })
 
     describe('reading messages from stream', () => {
       it('returns expected messages', async () => {
-        const found = []
-        for await (const message of store.read('stream-123')) {
-          found.push(message)
-        }
-        expect(found).toEqual([message1, message2])
+        const found = await read('stream-123')
+
+        expect(found.map(m => m.data)).toEqual([message1.data, message2.data])
       })
     })
 
     describe('reading messages from stream position', () => {
       it('returns expected subset of messages', async () => {
-        const found = []
-        for await (const message of store.read('stream-123', 1)) {
-          found.push(message)
-        }
-        expect(found).toEqual([message2])
+        const found = await read('stream-123', 1)
+
+        expect(found.map(m => m.data)).toEqual([message2.data])
       })
     })
 
     describe('reading messages from other stream', () => {
       it('returns no messages', async () => {
-        const found = []
-        for await (const message of store.read('stream-666')) {
-          found.push(message)
-        }
+        const found = await read('stream-666')
+
         expect(found).toHaveLength(0)
       })
     })
   })
 
   describe('write batch of messages to stream', () => {
-    let store, message1, message2
-    beforeEach(() => {
+    let message1, message2
+    beforeEach(async () => {
       message1 = exampleMessageData()
       message2 = exampleMessageData()
-      store = createMessageStore()
-      store.write([message1, message2], 'stream-123')
+      await store.write([message1, message2], 'stream-123')
     })
 
     describe('reading messages from stream', () => {
       it('returns expected messages', async () => {
-        const found = []
-        for await (const message of store.read('stream-123')) {
-          found.push(message)
+        const found = await read('stream-123')
+
+        expect(found.map(m => m.data)).toEqual([message1.data, message2.data])
+      })
+    })
+  })
+
+  describe('positions', () => {
+    describe('within a stream', () => {
+      let found
+      beforeEach(async () => {
+        const streamName = exampleStreamName()
+        await store.write([exampleMessageData(), exampleMessageData()], streamName)
+
+        found = await read(streamName)
+      })
+
+      it('position increases', () => {
+        expect(found.map(m => m.position)).toEqual([0, 1])
+      })
+
+      it('globalPosition increases', () => {
+        expect(found.map(m => m.globalPosition)).toEqual([0, 1])
+      })
+    })
+
+    describe('across streams', () => {
+      let found
+      beforeEach(async () => {
+        const streamName1 = exampleStreamName()
+        await store.write([exampleMessageData(), exampleMessageData()],
+          streamName1)
+
+        const streamName2 = exampleStreamName()
+        await store.write([exampleMessageData(), exampleMessageData()],
+          streamName2)
+
+        found = await read(streamName2)
+      })
+
+      it('position resets', () => {
+        expect(found.map(m => m.position)).toEqual([0, 1])
+      })
+
+      it('globalPosition increases', () => {
+        expect(found.map(m => m.globalPosition)).toEqual([2, 3])
+      })
+    })
+  })
+
+  describe('duplicate message id', () => {
+    it('is an error', async () => {
+      const streamName = exampleStreamName()
+      const message = exampleMessageData()
+      await store.write(message, streamName)
+
+      await expect(store.write(message, streamName)).rejects.toEqual(new Error(
+        `message-store write: duplicate message id: ${message.id}`))
+    })
+  })
+
+  describe('expected version', () => {
+    describe('writing multiple messages in same category', () => {
+      it('messages are written with the expected versions', async () => {
+        const streamName = exampleStreamName()
+
+        const wm0 = exampleMessageData()
+        await store.write(wm0, streamName)
+
+        const wm1 = exampleMessageData()
+        await store.write(wm1, streamName)
+
+        const readMessage = (await read(streamName, 1))[0]
+
+        expect(readMessage.data).toEqual(wm1.data)
+      })
+    })
+
+    describe('writing message with stale version', () => {
+      let streamName, error, sharedStore
+      beforeAll(async () => {
+        streamName = exampleStreamName()
+        sharedStore = createMessageStore()
+        const oldPosition = await sharedStore.write(exampleMessageData(), streamName)
+        await sharedStore.write(exampleMessageData(), streamName)
+
+        try {
+          await sharedStore.write(exampleMessageData(), streamName, oldPosition)
+        } catch (e) {
+          error = e
         }
-        expect(found).toEqual([message1, message2])
+      })
+
+      it('results in an error', async () => {
+        const expectedMessage = `message-store write: Wrong expected version: 0 (Stream: ${streamName}, Stream Version: 1)`
+        expect(error).toEqual(new Error(expectedMessage))
+      })
+
+      it('does not write new message', async () => {
+        const results = await readFrom(sharedStore, streamName)
+        expect(results.length).toBe(2)
       })
     })
   })
