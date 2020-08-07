132b1b49fd89d1977e733a6a58fb53928bf6b335
Mario Pareja <pareja.mario@gmail.com>
2020-08-07 07:41:32 -0400

    message-store: read: specify default batchSize & allow per-request batchSize
    + ensure batchSize is always fowarded downward to ensure `get` and `read` batch sizes align

--- message-store/read.js
+++ message-store/read.js
@@ -2,20 +2,22 @@ const { StreamName } = require('./stream-name')
 
 // read implementation offered to message-store implementations that
 // retrieve messages in batches
-module.exports = ({ batchSize, get }) => {
+module.exports = ({ batchSize = 1000, get }) => {
   const read = async function * (streamName, options = {}) {
+    const localBatchSize = options.batchSize || batchSize
+
     let position = options.position
 
     let results
     do {
-      results = await get(streamName, { ...options, position })
+      results = await get(streamName, { batchSize: localBatchSize, ...options, position })
       for (const result of results) {
         position = StreamName.isCategory(streamName)
           ? result.globalPosition + 1
           : result.position + 1
         yield result
       }
-    } while (results.length >= batchSize)
+    } while (results.length >= localBatchSize)
     // Results should't exceed batchSize in normal configuration
     // since get & read batchSize should be the same. Play it safe
     // with an inequality here in case the batchSizes _do_ differ
--- message-store/test/read.test.js
+++ message-store/test/read.test.js
@@ -19,6 +19,65 @@ describe('read', () => {
     })
   })
 
+  describe('batchSize', () => {
+    describe('no batch size specified', () => {
+      it('uses global default of 1000', async () => {
+        const streamName = exampleStreamName()
+        const get = jest.fn().mockResolvedValue([])
+
+        const { read } = createRead({ get })
+        await asyncIterableToArray(read(streamName)) // process all records
+
+        expect(get).toHaveBeenCalledWith(streamName, expect.objectContaining({
+          batchSize: 1000
+        }))
+      })
+    })
+
+    describe('global batchSize specified', () => {
+      it('uses specified global batchSize', async () => {
+        const streamName = exampleStreamName()
+        const get = jest.fn().mockResolvedValue([])
+
+        const { read } = createRead({ batchSize: 2, get })
+        await asyncIterableToArray(read(streamName)) // process all records
+
+        expect(get).toHaveBeenCalledWith(streamName, expect.objectContaining({
+          batchSize: 2
+        }))
+      })
+    })
+
+    describe('batchSize specified for a particular request', () => {
+      it('uses specified request batchSize', async () => {
+        const streamName = exampleStreamName()
+        const get = jest.fn().mockResolvedValue([])
+
+        const { read } = createRead({ batchSize: 2, get })
+        await asyncIterableToArray(read(streamName, { batchSize: 10 })) // process all records
+
+        expect(get).toHaveBeenCalledWith(streamName, expect.objectContaining({
+          batchSize: 10
+        }))
+      })
+    })
+
+    describe('BUG: partial results because request batch size not used when deciding to continue', () => {
+      it('uses specified request batchSize', async () => {
+        const streamName = exampleStreamName()
+        const get = jest.fn()
+          .mockResolvedValueOnce([exampleReadMessageData(), exampleReadMessageData()])
+          .mockResolvedValueOnce([exampleReadMessageData(), exampleReadMessageData()])
+          .mockResolvedValueOnce([])
+
+        const { read } = createRead({ batchSize: 4, get })
+        const results = await asyncIterableToArray(read(streamName, { batchSize: 2 })) // process all records
+
+        expect(results).toHaveLength(4)
+      })
+    })
+  })
+
   describe('given a custom option', () => {
     it('custom option is forwarded to get implementation', async () => {
       const streamName = exampleStreamName()
@@ -28,7 +87,9 @@ describe('read', () => {
       const { read } = createRead({ batchSize: 10, get })
       await asyncIterableToArray(read(streamName, { customOption })) // process all records
 
-      expect(get).toHaveBeenCalledWith(streamName, { customOption })
+      expect(get).toHaveBeenCalledWith(streamName, expect.objectContaining({
+        customOption
+      }))
     })
   })
 })
